fcc847f39d03a7aa68d8fbb6961d2aa9ff45ddef
mtgmind@gmail.com <mtgmind@gmail.com@9ff22242-88e1-838c-ddce-55861fb80818>
2012-04-22 12:19:07 +0000

    Photo

--- rynkeblog/menu.php
+++ rynkeblog/menu.php
@@ -1,10 +1,11 @@
 	<div class="wrap">
 		<div id="logo">
 			<h1><a href="." title="Home">Umpa Lompa blog</a></h1>
-			<p>oblig3 webapplikasjoner</p>
+			<p>Prosjektoppgave webapplikasjoner</p>
 		</div>
 		<ul id="nav">
 			<li><a class="current" href="." accesskey="h"><span class="akey">H</span>ome</a></li>
+                        <li><a href="photo.php" accesskey="p"><span class="akey">P</span>hoto</a></li>
 			<li><a href="about.php" accesskey="a"><span class="akey">A</span>bout</a></li>
 			<li><a href="links.php" accesskey="m"><span class="akey">L</span>inks</a></li>
 			<!--li><a href="task.htm" accesskey="r"><span class="akey">T</span>ask</a></li-->
---
+++ rynkeblog/photo.php
@@ -0,0 +1,43 @@
+<?php session_start(); ?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+
+<html>
+<head>
+<title>Photo</title>
+<link rel="stylesheet" href="css/main.css" type="text/css" media="screen, projection" />
+<link rel="stylesheet" href="css/photostyle.css" type="text/css"  />
+</head>
+<body id="home">
+    <?php include_once 'menu.php'; ?>
+    
+    <div class="photo-album">
+        
+        <a href="photo/fall.jpg" class="large polaroid img1"><img src="photo/mini/fall.jpg" alt="">Narvik Fall</a>
+	<a href="photo/paris.jpg" class="large polaroid img2"><img src="photo/mini/paris.jpg" alt="">Effel Tower</a>
+	<a href="photo/mirror.jpg" class="large polaroid img3"><img src="photo/mini/mirror.jpg" alt="">Toettatoppen in the mirror</a>
+        <a href="photo/Narvik.jpg" class="large polaroid img4"><img src="photo/mini/Narvik.jpg" alt="">Narvik Fall</a>
+	<a href="photo/Prague.jpg" class="large polaroid img5"><img src="photo/mini/Prague.jpg" alt="">Effel Tower</a>
+	<a href="photo/bee.jpg" class="large polaroid img6"><img src="photo/mini/bee.jpg" alt="">Toettatoppen in the mirror</a>
+        <a href="photo/friends.jpg" class="large polaroid img7"><img src="photo/mini/friends.jpg" alt="">Narvik Fall</a>
+	<a href="photo/hdr.jpg" class="large polaroid img8"><img src="photo/mini/hdr.jpg" alt="">Effel Tower</a>
+	<a href="photo/macro.jpg" class="large polaroid img9"><img src="photo/mini/macro.jpg" alt="">Toettatoppen in the mirror</a>
+        <a href="photo/mountains.jpg" class="large polaroid img10"><img src="photo/mini/mountains.jpg" alt="">Narvik Fall</a>
+	<a href="photo/nightnarvik.jpg" class="large polaroid img11"><img src="photo/mini/nightnarvik.jpg" alt="">Effel Tower</a>
+	<a href="photo/northlight.jpg" class="large polaroid img12"><img src="photo/mini/northlight.jpg" alt="">Toettatoppen in the mirror</a>
+        <a href="photo/palace.jpg" class="large polaroid img13"><img src="photo/mini/palace.jpg" alt="">Narvik Fall</a>
+	<a href="photo/parade.jpg" class="large polaroid img14"><img src="photo/mini/parade.jpg" alt="">Effel Tower</a>
+	<a href="photo/ship.jpg" class="large polaroid img15"><img src="photo/mini/ship.jpg" alt="">Toettatoppen in the mirror</a>
+        <a href="photo/slalom.jpg" class="large polaroid img16"><img src="photo/mini/slalom.jpg" alt="">Narvik Fall</a>
+	<a href="photo/water.jpg" class="large polaroid img17"><img src="photo/mini/water.jpg" alt="">Effel Tower</a>
+	
+    </div>
+    
+    
+<div id="left">
+    <p><strong>Blog:</strong> <a href=".">Home</a>
+</div>	
+		
+		
+		
+</body>
+</html>
