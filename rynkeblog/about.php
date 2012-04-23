13f1dc54c1a2817769631613d68a1410721dcdce
elexer@gmail.com <elexer@gmail.com@9ff22242-88e1-838c-ddce-55861fb80818>
2012-04-23 08:34:58 +0000

    martin

--- rynkeblog/about.php
+++ rynkeblog/about.php
@@ -11,15 +11,11 @@
     <?php include_once 'menu.php'; ?>
 <div id="left">
 
-		<h2>About me</h2><br/><br/>
+		<h2>About the Blog</h2><br/><br/>
 		<img border="0" src="../images/martin.png)"  width="304" height="228" /><br/>
-			My name is Martin Bang Tollefsen, and i am from Lofoten-island svolv�r
-			I moved to Narvik in 2007 to enter high school. after graduation I got a job as an apprentice at Narvik high school.
-			After two years I got craft certificate in IT operations.
-			<p>When the apprenticeship was over, I started at Narvik universety college, on the y-road.</p>		
-			Today I go to school full time and work at Funn-IT as a support consultant. This is a very interesting and fun job.
-			<p>I also have a girlfriend that I've been with for over 5 years. We also have a daughter together, named Linnea-marine.
-			she is almost two years old.</p>
+			
+                
+                
 			
 			<p><strong>Blog:</strong> <a href=".">Home</a>
 		</div>	
