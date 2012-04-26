c6e1c0964e0f3d87e449f879ff5ea3a3c74032da
mtgmind@gmail.com <mtgmind@gmail.com@9ff22242-88e1-838c-ddce-55861fb80818>
2012-04-26 14:56:45 +0000

    Ferdig

--- rynkeblog/photo.php
+++ rynkeblog/photo.php
@@ -16,24 +16,24 @@
     
     <div class="photo-album">
         
-        <a href="photo/Narvik.jpg" class="large polaroid img1"><img src="photo/mini/Narvik.jpg" alt="">Narvik from "Tredje toppen"</a>
-	<a href="photo/fall.jpg" class="large polaroid img2"><img src="photo/mini/fall.jpg" alt="">Narvik Fall</a>
-        <a href="photo/mirror.jpg" class="large polaroid img3"><img src="photo/mini/mirror.jpg" alt="">Toettatoppen in the mirror</a>
-        <a href="photo/northlight.jpg" class="large polaroid img4"><img src="photo/mini/northlight.jpg" alt="">North light</a>
-        <a href="photo/nightnarvik.jpg" class="large polaroid img5"><img src="photo/mini/nightnarvik.jpg" alt="">Night in Narvik</a>
-        <a href="photo/slalom.jpg" class="large polaroid img6"><img src="photo/mini/slalom.jpg" alt="">Slalom from "Tredje toppen"</a>
-	<a href="photo/friends.jpg" class="large polaroid img7"><img src="photo/mini/friends.jpg" alt="">Friends</a>
-        <a href="photo/mountains.jpg" class="large polaroid img8"><img src="photo/mini/mountains.jpg" alt="">Mountains</a>
-        <a href="photo/ship.jpg" class="large polaroid img9"><img src="photo/mini/ship.jpg" alt="">Ship</a>
+        <a href="photo/Narvik.jpg" class="large polaroid img1"><img src="photo/mini/Narvik.jpg" alt="" />Narvik from "Tredje toppen"</a>
+	<a href="photo/fall.jpg" class="large polaroid img2"><img src="photo/mini/fall.jpg" alt="" />Narvik Fall</a>
+        <a href="photo/mirror.jpg" class="large polaroid img3"><img src="photo/mini/mirror.jpg" alt="" />Toettatoppen in the mirror</a>
+        <a href="photo/northlight.jpg" class="large polaroid img4"><img src="photo/mini/northlight.jpg" alt="" />North light</a>
+        <a href="photo/nightnarvik.jpg" class="large polaroid img5"><img src="photo/mini/nightnarvik.jpg" alt="" />Night in Narvik</a>
+        <a href="photo/slalom.jpg" class="large polaroid img6"><img src="photo/mini/slalom.jpg" alt="" />Slalom from "Tredje toppen"</a>
+	<a href="photo/friends.jpg" class="large polaroid img7"><img src="photo/mini/friends.jpg" alt="" />Friends</a>
+        <a href="photo/mountains.jpg" class="large polaroid img8"><img src="photo/mini/mountains.jpg" alt="" />Mountains</a>
+        <a href="photo/ship.jpg" class="large polaroid img9"><img src="photo/mini/ship.jpg" alt="" />Ship</a>
         
-        <a href="photo/paris.jpg" class="large polaroid img10"><img src="photo/mini/paris.jpg" alt="">Effel Tower</a>
-        <a href="photo/water.jpg" class="large polaroid img11"><img src="photo/mini/water.jpg" alt="">Waterfall</a>
-        <a href="photo/parade.jpg" class="large polaroid img12"><img src="photo/mini/parade.jpg" alt="">Independence Day parade</a>
-        <a href="photo/palace.jpg" class="large polaroid img13"><img src="photo/mini/palace.jpg" alt="">The Catherine Palace</a>
-        <a href="photo/hdr.jpg" class="large polaroid img14"><img src="photo/mini/hdr.jpg" alt="">View from Fagernes mountain</a>
-        <a href="photo/Prague.jpg" class="large polaroid img15"><img src="photo/mini/Prague.jpg" alt="">Beautiful Prague</a>
-        <a href="photo/bee.jpg" class="large polaroid img16"><img src="photo/mini/bee.jpg" alt="">Maja bee</a>
-        <a href="photo/macro.jpg" class="large polaroid img17"><img src="photo/mini/macro.jpg" alt="">Drop of water</a>
+        <a href="photo/paris.jpg" class="large polaroid img10"><img src="photo/mini/paris.jpg" alt="" />Effel Tower</a>
+        <a href="photo/water.jpg" class="large polaroid img11"><img src="photo/mini/water.jpg" alt="" />Waterfall</a>
+        <a href="photo/parade.jpg" class="large polaroid img12"><img src="photo/mini/parade.jpg" alt="" />Independence Day parade</a>
+        <a href="photo/palace.jpg" class="large polaroid img13"><img src="photo/mini/palace.jpg" alt="" />The Catherine Palace</a>
+        <a href="photo/hdr.jpg" class="large polaroid img14"><img src="photo/mini/hdr.jpg" alt="" />View from Fagernes mountain</a>
+        <a href="photo/Prague.jpg" class="large polaroid img15"><img src="photo/mini/Prague.jpg" alt="" />Beautiful Prague</a>
+        <a href="photo/bee.jpg" class="large polaroid img16"><img src="photo/mini/bee.jpg" alt="" />Maja bee</a>
+        <a href="photo/macro.jpg" class="large polaroid img17"><img src="photo/mini/macro.jpg" alt="" />Drop of water</a>
         
     </div>	
 		
